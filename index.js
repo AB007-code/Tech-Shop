867f3fb41e9f4c4f41510a05f16326c3bfe67b7c
Abhilash <abhilash.vc888@gmail.com>
2025-02-22 01:40:25 +0530

    added

---
+++ index.js
@@ -0,0 +1,22 @@
+// let div = document.querySelector(".product");
+// // let arr = [];
+// const getProduct = async () => {
+//   let res = await fetch("https://fakestoreapi.com/products");
+//   let data = await res.json();
+//   data.forEach((ele) => {
+//     let div1 = document.createElement("div");
+//     let item = `<div class="border">
+//          <div><img src="${ele.image}" height="200px" width="200px"/></div>
+//          <div>${ele.title}</div>
+//          <div>${ele.description}</div>
+//          <div>${ele.price}</div>
+//          <div>
+//          <button class="btn btn-primary">Details</button>
+//          <button class="btn btn-primary">Add to Cart</button>
+//          </div>
+//       </div>`;
+//     div1.innerHTML = item;
+//     div.appendChild(div1);
+//   });
+// };
+// getProduct();
