c2a91b868755dc096078be091f010fe75b9c5e3e
Abhilash <abhilash.vc888@gmail.com>
2025-02-22 17:34:13 +0530

    added

--- index.js
+++ index.js
@@ -1,22 +1,26 @@
-// let div = document.querySelector(".product");
-// // let arr = [];
-// const getProduct = async () => {
-//   let res = await fetch("https://fakestoreapi.com/products");
-//   let data = await res.json();
-//   data.forEach((ele) => {
-//     let div1 = document.createElement("div");
-//     let item = `<div class="border">
-//          <div><img src="${ele.image}" height="200px" width="200px"/></div>
-//          <div>${ele.title}</div>
-//          <div>${ele.description}</div>
-//          <div>${ele.price}</div>
-//          <div>
-//          <button class="btn btn-primary">Details</button>
-//          <button class="btn btn-primary">Add to Cart</button>
-//          </div>
-//       </div>`;
-//     div1.innerHTML = item;
-//     div.appendChild(div1);
-//   });
-// };
-// getProduct();
+let img = document.querySelectorAll(".img");
+let brandName = document.querySelectorAll(".brandName");
+let dscrptnDiv = document.querySelectorAll(".dscrptnDiv");
+let priceDiv = document.querySelectorAll(".priceDiv");
+let homeBtn = document.querySelector(".homeBtn");
+let productBtn = document.querySelector(".productBtn");
+// let arr = [];
+const getProduct = async () => {
+  let res = await fetch("https://fakestoreapi.com/products");
+  let data = await res.json();
+  let i = 0;
+  data.forEach((ele) => {
+    let x = ele.title.slice(0, 12);
+    let y = ele.description.slice(0, 120);
+    img[i].src = `${ele.image}`;
+    brandName[i].textContent = `${x}...`;
+    dscrptnDiv[i].textContent = `${y}...`;
+    priceDiv[i].textContent = `$ ${ele.price}`;
+    i++;
+  });
+};
+getProduct();
+homeBtn.addEventListener("click", () => {
+  getProduct();
+});
+// productBtn.addEventListener("click", () => {});
