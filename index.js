95a21c43793996905d3c81d04e7a3c2cea9eec78
Abhilash <abhilash.vc888@gmail.com>
2025-02-27 01:43:31 +0530

    added

--- index.js
+++ index.js
@@ -1,26 +1,68 @@
-let img = document.querySelectorAll(".img");
-let brandName = document.querySelectorAll(".brandName");
-let dscrptnDiv = document.querySelectorAll(".dscrptnDiv");
-let priceDiv = document.querySelectorAll(".priceDiv");
+let row1 = document.querySelector(".row1");
 let homeBtn = document.querySelector(".homeBtn");
 let productBtn = document.querySelector(".productBtn");
-// let arr = [];
+let allBtn = document.querySelector(".allBtn");
+let mBtn = document.querySelector(".mBtn");
+let wBtn = document.querySelector(".wBtn");
+let jBtn = document.querySelector(".jBtn");
+let eBtn = document.querySelector(".eBtn");
+let arr = [];
 const getProduct = async () => {
   let res = await fetch("https://fakestoreapi.com/products");
   let data = await res.json();
-  let i = 0;
+  productList(data);
   data.forEach((ele) => {
-    let x = ele.title.slice(0, 12);
-    let y = ele.description.slice(0, 120);
-    img[i].src = `${ele.image}`;
-    brandName[i].textContent = `${x}...`;
-    dscrptnDiv[i].textContent = `${y}...`;
-    priceDiv[i].textContent = `$ ${ele.price}`;
-    i++;
+    arr.push(ele);
   });
 };
 getProduct();
+console.log(arr);
+function productList(arr1) {
+  //Make a function for adding the item in a row according to the array condition.
+  let i = 0;
+  arr1.map((ele) => {
+    let x = ele.title.slice(0, 12);
+    let y = ele.description.slice(0, 120);
+    let colDiv = document.createElement("div");
+    colDiv.className = "col-4 cold";
+    let col = `<div class="itembox border rounded">
+            <div class="imgDiv">
+              <img src="${ele.image}" class="img" height="280px" width="80%" />
+            </div>
+            <div><h4 class="brandName">${x}...</h4></div>
+            <div class="dscrptnDiv">${y}</div>
+            <div class="border-top border-bottom py-1 fs-3 priceDiv">$ ${ele.price}</div>
+            <div class="p-1">
+              <button class="btn btn-dark">Details</button>
+              <button class="btn btn-dark">Add to Cart</button>
+            </div>
+          </div>`;
+    colDiv.innerHTML = col;
+    row1.appendChild(colDiv);
+    i++;
+  });
+}
+
 homeBtn.addEventListener("click", () => {
   getProduct();
+});
+
+allBtn.addEventListener("click", () => {
+  let list1 = document.querySelectorAll(".cold");
+  list1.forEach((ele) => {
+    row1.removeChild(ele);
+  });
+  productList(arr);
+});
+mBtn.addEventListener("click", () => {
+  let list1 = document.querySelectorAll(".cold");
+  let arr2 = arr.filter((ele, i) => {
+    if (ele.category == "men's clothing") {
+      return ele;
+    } else {
+      row1.removeChild(list1[i]);
+    }
+  });
+  productList(arr2);
 });
 // productBtn.addEventListener("click", () => {});
